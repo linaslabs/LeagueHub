1c08bdbc6b5f94522116c73354387268a93bd0d4
Abel Linas <linasabel@gmail.com>
2024-12-12 11:46:13 +0000

    Added implementation of node.js and express.js with server.js and included initial api fetch/test

---
+++ public/index.js
@@ -0,0 +1,4 @@
+fetch("http://127.0.0.1:3000/api/standings")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+const standingsApi = "http://api.football-data.org/v4/competitions/PL/standings"
+const apiToken = "a8c7a2fb7ec9423c88904885ed9d082c"
+
+// Probably dont need this anymore
+app.use(
+    cors({
+    origin: "http://127.0.0.1:3000",
+    })
+)
+
+app.get('/api/standings',async (req,res) =>{
+    const response = await fetch(standingsApi, {
+        headers:{
+            "X-Auth-Token" : apiToken,
+        }
+    })
+    const data = await response.json()
+    res.json(data)
+})
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.listen(3000)
+
+
+// error page not found?
+// app.use((req,res) =>{
+//     res.status(404).sendFile("./public/404.html", { root: __dirname})
+// })
